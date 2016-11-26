5ea7a67b516ddea772bd93d7f4ff6eae5e84b857
Marcel Greter <marcel.greter@ocbnet.ch>
2016-11-26 14:16:50 +0100

    Remove superfluous method calls in context

--- src/context.cpp
+++ src/context.cpp
@@ -651,11 +651,11 @@ namespace Sass {
     Cssize cssize(*this, &backtrace);
     CheckNesting check_nesting;
     // check nesting
-    root->perform(&check_nesting)->block();
+    root->perform(&check_nesting);
     // expand and eval the tree
     root = root->perform(&expand)->block();
     // check nesting
-    root->perform(&check_nesting)->block();
+    root->perform(&check_nesting);
     // merge and bubble certain rules
     root = root->perform(&cssize)->block();
     // should we extend something?
