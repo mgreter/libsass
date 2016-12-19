e0154ec6c95645b0d171d4245ef90ed8817393f3
Marcel Greter <marcel.greter@ocbnet.ch>
2016-12-19 15:11:27 +0100

    Expose variable environments via C-API

--- src/eval.cpp
+++ src/eval.cpp
@@ -884,7 +884,8 @@ namespace Sass {
         c->pstate().path,
         c->pstate().line + 1,
         c->pstate().column + 1,
-        SASS_CALLEE_FUNCTION
+        SASS_CALLEE_FUNCTION,
+        { env }
       });
 
       // eval the body if user-defined or special, invoke underlying CPP function if native
@@ -924,7 +925,8 @@ namespace Sass {
         c->pstate().path,
         c->pstate().line + 1,
         c->pstate().column + 1,
-        SASS_CALLEE_C_FUNCTION
+        SASS_CALLEE_C_FUNCTION,
+        { env }
       });
 
       To_C to_c;
