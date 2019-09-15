62104ecc59f4768e5bd23e88a5ae5dde5ed72f59
Marcel Greter <marcel.greter@ocbnet.ch>
2019-09-16 00:51:09 +0200

    Add missing file

---
+++ src/settings.hpp
@@ -0,0 +1,12 @@
+#ifndef SASS_SETTINGS_H
+#define SASS_SETTINGS_H
+
+// Shared compile time settings should go here
+
+// When enabled we use our custom memory pool allocator
+// With intense workloads this can double the performance
+// Max memory usage mostly only grows by a slight amount
+#define SASS_CUSTOM_ALLOCATOR
+
+
+#endif
