8531cc20b16c1fd352f3d41e38058bb6ca2dd978
Marcel Greter <marcel.greter@ocbnet.ch>
2020-11-16 23:57:48 +0100

    Still 18

--- src/environment.cpp
+++ src/environment.cpp
@@ -1361,18 +1361,10 @@ namespace Sass {
       // the scope opened by the `a` style rule.
       // Create 
 
-      if (true || !sheet->root2->mergedFwdVar.empty() || !sheet->root2->mergedFwdFn.empty() || !sheet->root2->mergedFwdMix.empty()) {
-        auto newrefs = new VarRefs(compiler.varRoot, nullptr, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, false, false, false);
-        for (auto asd : sheet->root2->mergedFwdVar) { newrefs->varIdxs.insert(asd); }
-        for (auto asd : sheet->root2->mergedFwdMix) { newrefs->mixIdxs.insert(asd); }
-        for (auto asd : sheet->root2->mergedFwdFn) { newrefs->fnIdxs.insert(asd); }
-        for (auto asd : sheet->root2->idxs->varIdxs) { newrefs->varIdxs.insert(asd); }
-        for (auto asd : sheet->root2->idxs->mixIdxs) { newrefs->mixIdxs.insert(asd); }
-        for (auto asd : sheet->root2->idxs->fnIdxs) { newrefs->fnIdxs.insert(asd); }
-        pframe->fwdGlobal55.insert(
-          pframe->fwdGlobal55.begin(),
-          newrefs);
-      }
+      sheet->root2->idxs->module = sheet->root2;
+      pframe->fwdGlobal55.insert(
+        pframe->fwdGlobal55.begin(),
+        sheet->root2->idxs);
 
     }
 
