6be01cab25b1e3b7b619abdc382fd0920221aafe
Marcel Greter <marcel.greter@ocbnet.ch>
2020-11-16 14:24:52 +0100

    Down to 12

--- src/eval.cpp
+++ src/eval.cpp
@@ -2961,18 +2961,33 @@ namespace Sass {
       std::cerr << "IMPORT HAS NEW VAR " << asd.first.orig() << "\n";
     }
 
-    if (true || !sheet->root2->mergedFwdVar.empty() || !sheet->root2->mergedFwdFn.empty() || !sheet->root2->mergedFwdMix.empty()) {
-      auto newrefs = new VarRefs(compiler.varRoot, refs->pscope, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, false, false, false);
-      for (auto asd : sheet->root2->mergedFwdVar) { newrefs->varIdxs.insert(asd); }
-      for (auto asd : sheet->root2->idxs->varIdxs) { newrefs->varIdxs.insert(asd); }
-      for (auto asd : sheet->root2->mergedFwdMix) { newrefs->mixIdxs.insert(asd); }
-      for (auto asd : sheet->root2->idxs->mixIdxs) { newrefs->mixIdxs.insert(asd); }
-      for (auto asd : sheet->root2->mergedFwdFn) { newrefs->fnIdxs.insert(asd); }
-      for (auto asd : sheet->root2->idxs->fnIdxs) { newrefs->fnIdxs.insert(asd); }
-      pframe->fwdGlobal55.insert(
-        pframe->fwdGlobal55.begin(),
-        std::make_pair(newrefs, sheet->root2));
+    if (pframe->varFrame == 0xFFFFFFFF) {
+      // Global can simply be exposed without further ado (same frame)
+      for (auto asd : sheet->root2->idxs->varIdxs) { pframe->varIdxs.insert(asd); }
+      for (auto asd : sheet->root2->idxs->mixIdxs) { pframe->mixIdxs.insert(asd); }
+      for (auto asd : sheet->root2->idxs->fnIdxs) { pframe->fnIdxs.insert(asd); }
+      for (auto asd : sheet->root2->mergedFwdVar) { pframe->varIdxs.insert(asd); }
+      for (auto asd : sheet->root2->mergedFwdMix) { pframe->mixIdxs.insert(asd); }
+      for (auto asd : sheet->root2->mergedFwdFn) { pframe->fnIdxs.insert(asd); }
+
     }
+    else {
+
+      if (true || !sheet->root2->mergedFwdVar.empty() || !sheet->root2->mergedFwdFn.empty() || !sheet->root2->mergedFwdMix.empty()) {
+        auto newrefs = new VarRefs(compiler.varRoot, refs->pscope, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, false, false, false);
+        for (auto asd : sheet->root2->mergedFwdVar) { newrefs->varIdxs.insert(asd); }
+        for (auto asd : sheet->root2->idxs->varIdxs) { newrefs->varIdxs.insert(asd); }
+        for (auto asd : sheet->root2->mergedFwdMix) { newrefs->mixIdxs.insert(asd); }
+        for (auto asd : sheet->root2->idxs->mixIdxs) { newrefs->mixIdxs.insert(asd); }
+        for (auto asd : sheet->root2->mergedFwdFn) { newrefs->fnIdxs.insert(asd); }
+        for (auto asd : sheet->root2->idxs->fnIdxs) { newrefs->fnIdxs.insert(asd); }
+        pframe->fwdGlobal55.insert(
+          pframe->fwdGlobal55.begin(),
+          std::make_pair(newrefs, sheet->root2));
+      }
+
+    }
+
 
     // These data object have just been borrowed
     // sass_import_take_source(compiler.import_stack.back());
