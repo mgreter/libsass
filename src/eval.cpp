80bbab457741b1ebe48e53b4b1705453d32cba57
Marcel Greter <marcel.greter@ocbnet.ch>
2020-11-16 19:55:29 +0100

    OK, 13, but extremly slow!

--- src/eval.cpp
+++ src/eval.cpp
@@ -1963,6 +1963,7 @@ namespace Sass {
 
   Value* Eval::visitForwardRule(ForwardRule* node)
   {
+
     BackTrace trace(node->pstate(), Strings::forwardRule, false);
     callStackFrame frame(logger456, trace);
 
@@ -3013,16 +3014,16 @@ namespace Sass {
     else {
 
       if (true || !sheet->root2->mergedFwdVar.empty() || !sheet->root2->mergedFwdFn.empty() || !sheet->root2->mergedFwdMix.empty()) {
-//        auto newrefs = new VarRefs(compiler.varRoot, refs->pscope, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, false, false, false);
-//        for (auto asd : sheet->root2->mergedFwdVar) { newrefs->varIdxs.insert(asd); }
-//        for (auto asd : sheet->root2->mergedFwdMix) { newrefs->mixIdxs.insert(asd); }
-//        for (auto asd : sheet->root2->mergedFwdFn) { newrefs->fnIdxs.insert(asd); }
-//        for (auto asd : sheet->root2->idxs->varIdxs) { newrefs->varIdxs.insert(asd); }
-//        for (auto asd : sheet->root2->idxs->mixIdxs) { newrefs->mixIdxs.insert(asd); }
-//        for (auto asd : sheet->root2->idxs->fnIdxs) { newrefs->fnIdxs.insert(asd); }
-//        pframe->fwdGlobal55.insert(
-//          pframe->fwdGlobal55.begin(),
-//          std::make_pair(newrefs, sheet->root2));
+        auto newrefs = new VarRefs(compiler.varRoot, nullptr, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, false, false, false);
+        for (auto asd : sheet->root2->mergedFwdVar) { newrefs->varIdxs.insert(asd); }
+        for (auto asd : sheet->root2->mergedFwdMix) { newrefs->mixIdxs.insert(asd); }
+        for (auto asd : sheet->root2->mergedFwdFn) { newrefs->fnIdxs.insert(asd); }
+        for (auto asd : sheet->root2->idxs->varIdxs) { newrefs->varIdxs.insert(asd); }
+        for (auto asd : sheet->root2->idxs->mixIdxs) { newrefs->mixIdxs.insert(asd); }
+        for (auto asd : sheet->root2->idxs->fnIdxs) { newrefs->fnIdxs.insert(asd); }
+        pframe->fwdGlobal55.insert(
+          pframe->fwdGlobal55.begin(),
+          std::make_pair(newrefs, sheet->root2));
       }
 
     }
