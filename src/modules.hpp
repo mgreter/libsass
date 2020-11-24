d5f675178f7940bdc34ba808bda99c8010ff7617
Marcel Greter <marcel.greter@ocbnet.ch>
2020-11-24 21:55:30 +0100

    Damn

--- src/modules.hpp
+++ src/modules.hpp
@@ -110,7 +110,6 @@ namespace Sass {
     void addVariable(const EnvKey& name, uint32_t offset);
     void addMixin(const EnvKey& name, uint32_t offset);
     BuiltInMod(EnvRoot& root);
-    ~BuiltInMod();
   };
 
 }
